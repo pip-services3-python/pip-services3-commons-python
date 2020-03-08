cfe099e9dfa084af3ed705810c01c4e3c9254677
Danil <judas.priest999@gmail.com>
2020-03-08 15:30:37 +0300

    add MultiString class and tests

---
+++ test/data/test_MultiString.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+import pytest
+
+from pip_services3_commons.data.MultiString import MultiString
+
+
+class TestMultiString:
+
+    def test_crud(self):
+        multi_string = MultiString()
+        multi_string.append({'fr': 'Bonjour tout le monde!'})
+        multi_string.append({'ru': 'Привет мир!'})
+        multi_string.append({'en': 'do you speak english'})
+
+        assert multi_string == {'fr': 'Bonjour tout le monde!',
+                                'ru': 'Привет мир!',
+                                'en': 'do you speak english'}
+
+        assert multi_string.getLanguages() == ['fr', 'ru', 'en']
+        assert multi_string.get('en') == 'do you speak english'
+        multi_string.remove('en')
+        assert multi_string.getLanguages() == ['fr', 'ru']
+
+    def test_multistring_from(self):
+
+        from_tuples = MultiString.fromTuples('en', 'do you speak english',
+                                             'fr', 'parle français',
+                                             'de', 'sprichst du deutsch'
+                                             )
+        assert len(from_tuples) == 3
+        assert from_tuples == {'en': 'do you speak english',
+                               'fr': 'parle français',
+                               'de': 'sprichst du deutsch'}
+        from_value = MultiString.fromValue({'en': 'do you speak english'})
+        assert from_value == {'en': 'do you speak english'}
