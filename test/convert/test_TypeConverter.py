91d9d988039208863c06c38021df2c7487e9e63e
Danil <judas.priest999@gmail.com>
2020-03-13 20:34:39 +0300

    some fix test_TypeConverter and uncomment

--- test/convert/test_TypeConverter.py
+++ test/convert/test_TypeConverter.py
@@ -18,7 +18,7 @@ class TestTypeConverter:
     def test_to_type_code(self):
         assert TypeCode.String == TypeConverter.to_type_code(str)
         assert TypeCode.Integer == TypeConverter.to_type_code(int)
-        # assert TypeCode.Long == TypeConverter.to_type_code(long)
+
         assert TypeCode.Float == TypeConverter.to_type_code(float)
         assert TypeCode.DateTime == TypeConverter.to_type_code(datetime)
         assert TypeCode.Array == TypeConverter.to_type_code(list)
@@ -30,7 +30,6 @@ class TestTypeConverter:
 
         assert TypeCode.String == TypeConverter.to_type_code("123")
         assert TypeCode.Integer == TypeConverter.to_type_code(123)
-        # assert TypeCode.Long == TypeConverter.to_type_code(123)
         assert TypeCode.Float == TypeConverter.to_type_code(123.456)
         assert TypeCode.DateTime == TypeConverter.to_type_code(datetime(1975, 4, 8))
         assert TypeCode.Array == TypeConverter.to_type_code([])
@@ -42,7 +41,7 @@ class TestTypeConverter:
     def test_to_nullable_type(self):
         assert "123" == TypeConverter.to_nullable_type(str, 123)
         assert 123 == TypeConverter.to_nullable_type(int, "123")
-        # assert 123 == TypeConverter.to_nullable_type(long, 123.456)
+        assert 123 == TypeConverter.to_nullable_type(TypeCode.Long, 123.456)
         assert 0.001 > 123 - TypeConverter.to_nullable_type(float, 123)
         assert DateTimeConverter.to_datetime("1975-04-08T17:30:00.00Z") == TypeConverter.to_nullable_type(datetime, "1975-04-08T17:30:00.00Z")
         assert 1 == len(TypeConverter.to_nullable_type(list, 123))
@@ -52,7 +51,7 @@ class TestTypeConverter:
     def test_to_type(self):
         assert "123" == TypeConverter.to_type(str, 123)
         assert 123 == TypeConverter.to_type(int, "123")
-        # assert 123 == TypeConverter.to_type(long, 123.456)
+        assert 123 == TypeConverter.to_type(TypeCode.Long, 123.456)
         assert 0.001 > 123 - TypeConverter.to_type(float, 123)
         assert DateTimeConverter.to_datetime("1975-04-08T17:30:00.00Z") == TypeConverter.to_type(datetime, "1975-04-08T17:30:00.00Z")
         assert 1 == len(TypeConverter.to_type(list, 123))
@@ -62,7 +61,7 @@ class TestTypeConverter:
     def test_to_type_with_default(self):
         assert "123" == TypeConverter.to_type_with_default(str, None, "123")
         assert 123 == TypeConverter.to_type_with_default(int, None, 123)
-        # assert 123 == TypeConverter.to_type_with_default(long, None, 123)
+        assert 123 == TypeConverter.to_type_with_default(TypeCode.Long, None, 123)
         assert 0.001 > 123 - TypeConverter.to_type_with_default(float, None, 123.)
         assert DateTimeConverter.to_datetime("1975-04-08T17:30:00.00Z") == TypeConverter.to_type_with_default(datetime, "1975-04-08T17:30:00.00Z", None)
         assert 1 == len(TypeConverter.to_type_with_default(list, 123, None))
