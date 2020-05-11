6049caf125a970a024833cc2e52c58007d351838
afmmars <anastasf/2gmail.com>
2020-05-11 12:57:37 +0300

    update clean

--- clean.ps1
+++ clean.ps1
@@ -15,13 +15,13 @@ docker rmi $testImage --force
 docker image prune --force
 
 # remove cash and temp files 
-# Remove-Item -Recurse -Force .cache
-# Remove-Item -Recurse -Force dist
-# Remove-Item -Recurse -Force pip_services3_commons.egg-info
-# Remove-Item -Force pip_services3_commons/*.pyc
-# Remove-Item -Force pip_services3_commons/**/*.pyc
-# Remove-Item -Recurse -Force test/__pycache__
-# Remove-Item -Recurse -Force test/**/__pycache__
+Remove-Item -Recurse -Force .cache
+Remove-Item -Recurse -Force dist
+Remove-Item -Recurse -Force pip_services3_commons.egg-info
+Remove-Item -Force pip_services3_commons/*.pyc
+Remove-Item -Force pip_services3_commons/**/*.pyc
+Remove-Item -Recurse -Force test/__pycache__
+Remove-Item -Recurse -Force test/**/__pycache__
 
 # Remove existed containers
 docker ps -a | Select-String -Pattern "Exit" | foreach($_) { docker rm $_.ToString().Split(" ")[0] }
