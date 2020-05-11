1cd0249b5ab3931f6dee10cc10cea6fe895c2ea7
afmmars <anastasf/2gmail.com>
2020-05-11 12:12:52 +0300

    update dockerization

--- release.ps1
+++ release.ps1
@@ -4,10 +4,9 @@ Set-StrictMode -Version latest
 $ErrorActionPreference = "Stop"
 
 $component = Get-Content -Path "component.json" | ConvertFrom-Json
-$version = (Get-Content -Path component.json | ConvertFrom-Json).version
 
-if ($component.version -ne $version) {
-    throw "Versions in component.json and package.json do not match"
+if ([string]::IsNullOrEmpty($component.version)) {
+    throw "Versions in component.json do not set"
 }
 
 
