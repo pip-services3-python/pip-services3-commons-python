65863ce33317ca9251f081c37be72d930a4ce67e
banalna <judas.priest999@gmail.com>
2021-03-20 01:11:13 +0300

    version up to 3.2.1

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ except:
 
 setup(
     name='pip_services3_commons',
-    version='3.2.0',
+    version='3.2.1',
     url='http://github.com/pip-services3-python/pip-services3-commons-python',
     license='MIT',
     description='Basic portable abstractions for Pip.Services in Python',
